1ac5ae256c042bdc2c4d7e4e20ace192267c8dcf
bibha <bibhashkalita94@gmail.com>
2023-07-30 08:55:31 +0530

    Mongodb Files Component with redirects

--- mongodb.sh
+++ mongodb.sh
@@ -3,7 +3,7 @@ echo -e "\e[33m copy Mongodb Repo file\e[0m"
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 echo -e "\e[33m Installing Mongodb server\e[0m"
 yum install mongodb-org -y  &>>/tmp/roboshop.log
-
+#Modify the Config file
 echo -e "\e[33m Start Mongodb Service\e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
 systemctl restart mongod &>>/tmp/roboshop.log
