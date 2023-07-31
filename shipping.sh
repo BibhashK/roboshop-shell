13479d9e1ec460266ba572f18640a50649a7ce55
bibha <bibhashkalita94@gmail.com>
2023-07-31 14:17:41 +0530

    Mongodb Files Component with redirects

--- shipping.sh
+++ shipping.sh
@@ -1,34 +1,34 @@
 echo -e "\e[33m Install Maven \e[0m"
-yum install maven -y
+yum install maven -y &>>/tmp/roboshop.log
 
 echo -e "\e[33m Add Application User \e[0m"
-useradd roboshop
+useradd roboshop &>>/tmp/roboshop.log
 
 echo -e "\e[33m Create Application Directory \e[0m"
-rm -rf /app
+rm -rf /app &>>/tmp/roboshop.log
 mkdir /app
 
 echo -e "\e[33m Download Application Content\e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>/tmp/roboshop.log
 cd /app
 
 echo -e "\e[33m Extract Application Content \e[0m"
-unzip /tmp/shipping.zip
+unzip /tmp/shipping.zip &>>/tmp/roboshop.log
 
 echo -e "\e[33m cDownload Maven Dependencies\e[0m"
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
+mvn clean package &>>/tmp/roboshop.log
+mv target/shipping-1.0.jar shipping.jar &>>/tmp/roboshop.log
 
 echo -e "\e[33m Insatll Mysql Client \e[0m"
-yum install mysql -y
+yum install mysql -y &>>/tmp/roboshop.log
 
 echo -e "\e[33m Load Schema \e[0m"
-mysql -h mysql-dev.devopsb2023.store -uroot -pRoboShop@1 < /app/schema/shipping.sql
+mysql -h mysql-dev.devopsb2023.store -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>/tmp/roboshop.log
 
 echo -e "\e[33m Start Shipping Service \e[0m"
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable shipping &>>/tmp/roboshop.log
+systemctl restart shipping &>>/tmp/roboshop.log
 
 
 
